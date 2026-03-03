38ea18ff51ba8e37f42a1161eacbe700dfa92aaa
Adityaomer <rochanaomer786@gmail.com>
2026-03-03 14:14:49 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 import os
 import pymongo
-from flask import jsonify
+from flask import Flask, jsonify
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
